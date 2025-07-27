17400bfaa7baf28a0492c4c37a511f44a0677d06
Jack <jack20220723@gmail.com>
2025-07-27 17:23:40 +0800

    Lift up restriction on dark-only theme in code snippet

--- src/config.ts
+++ src/config.ts
@@ -89,7 +89,6 @@ export const licenseConfig: LicenseConfig = {
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
-	theme: "github-dark",
+	// https://expressive-code.com/guides/themes/#available-themes
+	theme: "catppuccin-latte",
 };
